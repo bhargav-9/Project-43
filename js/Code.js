269bd9c1b797d8f81e76586efa76fa85c42869cd
bhargav-9 <74187333+bhargav-9@users.noreply.github.com>
2021-04-23 17:15:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("CONTIFON", 700,140)
+    fill("lightblue")
+    text("Hint: Procedure ", 700,160)
+
+    fill("white")
+    textSize(15)
+    text("ATADASBEs", 100,200)
+    fill("lightblue")
+    text("Hint: The Head ", 100,220)
+
+// add code for displaying rest of the names and the hints.
+    
+
+}
